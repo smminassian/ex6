7d27e3eea44b6ecf5f9d5dba0455bf8983d4980b
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-08 05:41:10 +0000

    added comments to tell my self where I left off

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 export default function Square() {
   return (
+    //Left off step before 123
+    //                     456
+    //                     789
     <>
       <button className="square">X</button>
       <button className="square">X</button>
