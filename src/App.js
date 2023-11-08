ca299a2df82e3c8f0c7768ef53cdc7fd0472b4fb
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-08 05:36:43 +0000

    Following what the tutorial is telling me to do. They want me to add 9 X's

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
 export default function Square() {
-  
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      <button className="square">X</button>
+
+      <button className="square">X</button>
+      
+    </>
+
+  );
 }
