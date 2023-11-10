cf558be79bd1918aa3fec544466c6da59dda3350
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-10 02:07:09 +0000

    It did not recognize "board_row" so I changed it to "board-row"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 export default function Square() {
   return (
     <>
-      <div class = "board_row">
+      <div class = "board-row">
         <button className="square">1</button>
         <button className="square">2</button>
         <button className="square">3</button>
       </div>
 
-      <div class = "board_row">
+      <div class = "board-row">
         <button className="square">4</button>
         <button className="square">5</button>
         <button className="square">6</button>
       </div>
       
-      <div class = "board_row">
+      <div class = "board-row">
         <button className="square">7</button>
         <button className="square">8</button>
         <button className="square">9</button>
