87b44619414d6394f5b2ad2927f89d1844d97266
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-10 02:36:19 +0000

    Completed another step

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ export default function Board() {
   return (
     <>
       <div class = "board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+       <Square />
+       <Square />
+       <Square />
       </div>
 
       <div class = "board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       
       <div class = "board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       
     </>
