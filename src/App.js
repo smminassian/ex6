284d880ecd5532a00b8bad530b2995a8ae4e385f
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-20 23:33:00 +0000

    made X pop up when u click a square

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import {useState} from 'react'
-
+import { useState } from "react";
 
 function Square() {
-  
   const [value, setValue] = useState(null);
 
-  
-  function handleClick(){
-    console.log('clicked');
+  function handleClick() {
+    setValue("X");
   }
   return (
-  <button 
-    className="square"
-    onClick = {handleClick}
-    >
-    {value}
+    <button className="square" onClick={handleClick}>
+      {value}
     </button>
   );
 }
@@ -23,20 +17,20 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
