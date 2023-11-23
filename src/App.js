a4b647c132afd5a245eeeaa5aa52bb8b8e7df322
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-23 02:46:44 +0000

    Added game function and set it as export default

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import { useState } from "react";
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
+export default function Game(){
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board />
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
