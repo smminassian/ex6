d3937378d24fab8629541be4d86e6b2270c16607
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-24 06:18:50 +0000

    trying to add calc into the port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
+import Calc from "./"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
