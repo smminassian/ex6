a22b749f8282335cb4f349c8f6be639b1aa388ac
sebastien <143156014+smminassian@users.noreply.github.com>
2023-11-24 06:33:57 +0000

    trying to add calculator css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import "./"
 
 import App from "./App";
 import Calc from "./calc";
